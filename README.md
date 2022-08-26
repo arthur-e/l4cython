85feb70b9bc24e5a15022e5a1874f6ed4059697b
K. Arthur Endsley <endsley@umich.edu>
2022-08-26 10:24:16 -0600

    Renamed extension files, removed GDB from compilation

--- README.md
+++ README.md
@@ -31,10 +31,11 @@ respiration = Extension(
     define_macros = [ # Avoids warning "Using deprecated NumPy API"
         ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')
     ], # https://stackoverflow.com/questions/52749662/using-deprecated-numpy-api
-    extra_compile_args = ['-g']
+    extra_compile_args = ['-g'],
+    extra_link_args = ['-g']
 )
 
-setup(ext_modules = cythonize(respiration, gdb_debug = True))
+setup(ext_modules = cythonize(respiration))
 ```
 
 [Then see this resource for tips on debugging.](https://github.com/cython/cython/wiki/DebuggingTechniques)
--- l4cython/setup.py
+++ l4cython/setup.py
@@ -4,13 +4,11 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 respiration = Extension(
-    name = 'respiration',
-    sources = ['respiration.pyx'],
+    name = 'reco',
+    sources = ['reco_9km.pyx'],
     define_macros = [ # Avoids warning "Using deprecated NumPy API"
         ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')
-    ], # https://stackoverflow.com/questions/52749662/using-deprecated-numpy-api
-    extra_compile_args = ['-g'],
-    extra_link_args = ['-g']
+    ] # https://stackoverflow.com/questions/52749662/using-deprecated-numpy-api
 )
 
-setup(ext_modules = cythonize(respiration, gdb_debug = True))
+setup(ext_modules = cythonize(respiration))
