e1bc13c631afa387da6f36658c65911ceb848d27
K. Arthur Endsley <endsley@umich.edu>
2022-08-29 09:47:58 -0600

    Minor changes to setup.py to facilitate further extensions

--- l4cython/setup.py
+++ l4cython/setup.py
@@ -3,12 +3,14 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-respiration = Extension(
+MACROS = [ # Avoids warning "Using deprecated NumPy API"
+    ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')
+] # https://stackoverflow.com/questions/52749662/using-deprecated-numpy-api
+
+reco = Extension(
     name = 'reco',
-    sources = ['reco.pyx'],
-    define_macros = [ # Avoids warning "Using deprecated NumPy API"
-        ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')
-    ] # https://stackoverflow.com/questions/52749662/using-deprecated-numpy-api
+    sources = ['reco_9km.pyx'],
+    define_macros = MACROS
 )
 
-setup(ext_modules = cythonize(respiration))
+setup(ext_modules = cythonize(reco))
