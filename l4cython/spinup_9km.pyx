b7e463fdab8b373e365ee2163d9eb99bdbff057c
K. Arthur Endsley <endsley@umich.edu>
2023-08-23 13:11:39 -0600

    Improved performance of analytical spin-up (spinup_9km.pyx)
    Changed from typed memory views to heap allocations and added a second "nogil" loop. Also changed AVAIL_NPP (formerly ANNUALL_NPP) to 1/365 the annual NPP sum for compatibility with future changes.

--- l4cython/spinup_9km.pyx
+++ l4cython/spinup_9km.pyx
@@ -15,6 +15,13 @@ accelerated decomposition:
 Accelerated decomposition rate coefficients of more than 100 tend to create
 unstable pixels in the steady-state image.
 
+Note that if the "data/NPP_annual_sum" configuration parameter is set to the
+empty string, the model will use the daily GPP (converted to NPP) to determine
+the daily NPP available for litterfall.
+
+Recent benchmarks on Gullveig (Intel Xeon 3.7 GHz): 15 secs for analytical
+spin-up, 36 min for numerical spin-up.
+
 Required data:
 
 - Surface soil wetness ("SMSF"), in percentage units [0,100]
@@ -26,10 +33,12 @@ import datetime
 import json
 import numpy as np
 from libc.math cimport isnan, fabs
+from libc.stdio cimport FILE, fread, fclose
 from cython.parallel import prange
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
 from l4cython.respiration cimport BPLUT, arrhenius, linear_constraint
-from l4cython.utils cimport to_numpy, to_numpy_double
+from l4cython.utils cimport open_fid, to_numpy, to_numpy_double
+from l4cython.utils.fixtures import READ
 from tqdm import tqdm
 
 # Number of grid cells in sparse ("land") arrays
@@ -43,22 +52,26 @@ cdef float KSTRUCT = 0.4 # Muliplier *against* base decay rate
 cdef float KRECAL = 0.0093
 
 # The PFT map
-cdef unsigned char PFT[SPARSE_N]
-cdef float ANNUAL_NPP[SPARSE_N]
+cdef:
+    FILE* fid
+    unsigned char* PFT
+    float* AVAIL_NPP
+PFT = <unsigned char*> PyMem_Malloc(sizeof(unsigned char) * SPARSE_N)
+AVAIL_NPP = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
 
 # Python arrays that want heap allocations must be global; this one is reused
 #   for any array that needs to be written to disk (using NumPy)
 OUT_M09_DOUBLE = np.full((SPARSE_N,), np.nan, dtype = np.float64)
 
-# L4_C BPLUT Version 6 (Vv6042, Vv6040, Nature Run v9.1)
+# L4_C BPLUT Version 7 (Vv7042, Vv7040, Nature Run v10)
 cdef BPLUT PARAMS
-PARAMS.smsf0[:] = [0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-PARAMS.smsf1[:] = [0, 25.0, 30.5, 39.8, 31.3, 44.9, 50.5, 25.0, 25.1]
-PARAMS.tsoil[:] = [0, 266.05, 392.24, 233.94, 265.23, 240.71, 261.42, 253.98, 281.69]
+PARAMS.smsf0[:] = [0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+PARAMS.smsf1[:] = [0, 25.0, 94.0, 42.3, 35.8, 44.9, 52.9, 25.0, 25.0]
+PARAMS.tsoil[:] = [0, 238.17, 422.77, 233.94, 246.48, 154.91, 366.14, 242.47, 265.06]
 PARAMS.cue[:] = [0, 0.687, 0.469, 0.755, 0.799, 0.649, 0.572, 0.708, 0.705]
 PARAMS.f_metabolic[:] = [0, 0.49, 0.71, 0.67, 0.67, 0.62, 0.76, 0.78, 0.78]
 PARAMS.f_structural[:] = [0, 0.3, 0.3, 0.7, 0.3, 0.35, 0.55, 0.5, 0.8]
-PARAMS.decay_rate[0] = [0, 0.020, 0.022, 0.031, 0.028, 0.013, 0.022, 0.019, 0.031]
+PARAMS.decay_rate[0] = [0, 0.020, 0.022, 0.030, 0.029, 0.012, 0.026, 0.018, 0.031]
 for p in range(1, 9):
     PARAMS.decay_rate[1][p] = PARAMS.decay_rate[0][p] * KSTRUCT
     PARAMS.decay_rate[2][p] = PARAMS.decay_rate[0][p] * KRECAL
@@ -143,23 +156,36 @@ cdef analytical_spinup(config, double* soc0, double* soc1, double* soc2):
         Py_ssize_t i
         Py_ssize_t doy
         int pft
+        float* smsf
+        float* tsoil
         float* f_met
         float* f_str
+        float* param_smsf0
+        float* param_smsf1
+        float* param_tsoil
         float* k0 # Decay rates of each pool
         float* k1
         float* k2
         float* k_mult
-        float w_mult[SPARSE_N]
-        float t_mult[SPARSE_N]
+        float* w_mult
+        float* t_mult
         float denom0, denom1
+    smsf = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
+    tsoil = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     f_met = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     f_str = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
+    param_smsf0 = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
+    param_smsf1 = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
+    param_tsoil = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     k0 = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     k1 = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     k2 = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     k_mult = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
+    w_mult = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
+    t_mult = <float*> PyMem_Malloc(sizeof(float) * SPARSE_N)
     print('Beginning analytical spin-up...')
-    # For each day of the climatological year
+    # Analytical spin-up calculation has a closed form, so this outer loop,
+    #   for each day of the climatological year, pre-computes annual sums
     for doy in tqdm(range(1, 366)):
         jday = str(doy).zfill(3)
         # NOTE: It will ALWAYS be faster to read these all-at-once rather than
@@ -167,10 +193,14 @@ cdef analytical_spinup(config, double* soc0, double* soc1, double* soc2):
         #   element one-at-a-time (as in load_state())
         # NOTE: For compatibility with TCF, the SMSF data are already in
         #   percentage units, i.e., on [0,100]
-        smsf = np.fromfile(
-            config['data']['climatology']['smsf'] % jday, dtype = np.float32)
-        tsoil = np.fromfile(
-            config['data']['climatology']['tsoil'] % jday, dtype = np.float32)
+        fid = open_fid((config['data']['climatology']['smsf'] % jday).encode('UTF-8'), READ)
+        fread(smsf, sizeof(float), <size_t>sizeof(float)*SPARSE_N, fid)
+        fclose(fid)
+        fid = open_fid((config['data']['climatology']['tsoil'] % jday).encode('UTF-8'), READ)
+        fread(tsoil, sizeof(float), <size_t>sizeof(float)*SPARSE_N, fid)
+        fclose(fid)
+        # Vectorization: This loop initializes arrays that represent the
+        #   parameter values for each pixel, to allow use in "nogil"
         for i in range(SPARSE_N):
             pft = int(PFT[i])
             if pft == 0 or pft > 8:
@@ -189,10 +219,18 @@ cdef analytical_spinup(config, double* soc0, double* soc1, double* soc2):
                     k2[i] = k2[i] * config['model']['ad_rate'][2]
                 f_met[i] = PARAMS.f_metabolic[pft]
                 f_str[i] = PARAMS.f_structural[pft]
+                param_smsf0[i] = PARAMS.smsf0[pft]
+                param_smsf1[i] = PARAMS.smsf1[pft]
+                param_tsoil[i] = PARAMS.tsoil[pft]
+        # Pre-compute the k_mult sum for each pixel
+        for i in prange(SPARSE_N, nogil = True):
+            pft = int(PFT[i])
+            if pft == 0 or pft > 8:
+                continue
             # Compute and increment annual k_mult sum
             w_mult[i] = linear_constraint(
-                smsf[i], PARAMS.smsf0[pft], PARAMS.smsf1[pft], 0)
-            t_mult[i] = arrhenius(tsoil[i], PARAMS.tsoil[pft], TSOIL1, TSOIL2)
+                smsf[i], param_smsf0[i], param_smsf1[i], 0)
+            t_mult[i] = arrhenius(tsoil[i], param_tsoil[i], TSOIL1, TSOIL2)
             k_mult[i] = k_mult[i] + (w_mult[i] * t_mult[i])
     # Outside of the daily loop, but for each pixel...
     for i in prange(SPARSE_N, nogil = True):
@@ -202,12 +240,12 @@ cdef analytical_spinup(config, double* soc0, double* soc1, double* soc2):
         # Calculate analytical steady-state of SOC
         denom0 = (k_mult[i] * k0[i])
         if denom0 > 0:
-            soc0[i] = <double>(ANNUAL_NPP[i] * f_met[i]) / denom0
+            soc0[i] = <double>(365 * AVAIL_NPP[i] * f_met[i]) / denom0
         else:
             soc0[i] = 0
         denom1 = (k_mult[i] * k1[i])
         if denom1 > 0:
-            soc1[i] = <double>(ANNUAL_NPP[i] * (1 - f_met[i])) / denom1
+            soc1[i] = <double>(365 * AVAIL_NPP[i] * (1 - f_met[i])) / denom1
         else:
             soc1[i] = 0
         # Guard against division by zero
@@ -215,12 +253,19 @@ cdef analytical_spinup(config, double* soc0, double* soc1, double* soc2):
             soc2[i] = <double>(f_str[i] * k1[i] * soc1[i]) / k2[i]
         else:
             soc2[i] = 0
+    PyMem_Free(smsf)
+    PyMem_Free(tsoil)
     PyMem_Free(f_met)
     PyMem_Free(f_str)
+    PyMem_Free(param_smsf0)
+    PyMem_Free(param_smsf1)
+    PyMem_Free(param_tsoil)
     PyMem_Free(k0)
     PyMem_Free(k1)
     PyMem_Free(k2)
     PyMem_Free(k_mult)
+    PyMem_Free(w_mult)
+    PyMem_Free(t_mult)
 
 
 cdef numerical_spinup(config, double* soc0, double* soc1, double* soc2):
@@ -286,8 +331,8 @@ cdef numerical_spinup(config, double* soc0, double* soc1, double* soc2):
         f_met[i] = PARAMS.f_metabolic[pft]
         f_str[i] = PARAMS.f_structural[pft]
         # Calculate annual GPP sum as (NPP / CUE)
-        gpp[i] = ANNUAL_NPP[i] / PARAMS.cue[pft]
-        ra_total[i] = gpp[i] - ANNUAL_NPP[i]
+        gpp[i] = (365 * AVAIL_NPP[i]) / PARAMS.cue[pft]
+        ra_total[i] = gpp[i] - (365 * AVAIL_NPP[i])
         k0[i] = PARAMS.decay_rate[0][pft]
         k1[i] = PARAMS.decay_rate[1][pft]
         k2[i] = PARAMS.decay_rate[2][pft]
@@ -340,7 +385,7 @@ cdef numerical_spinup(config, double* soc0, double* soc1, double* soc2):
                 # Compute one daily soil decomposition step for this pixel;
                 #   note that litterfall is 1/365 of the annual NPP sum
                 numerical_step(
-                    delta, rh_total, (ANNUAL_NPP[i] / 365), k_mult[i], f_met[i],
+                    delta, rh_total, AVAIL_NPP[i], k_mult[i], f_met[i],
                     f_str[i], k0[i], k1[i], k2[i], soc0[i], soc1[i], soc2[i])
                 # Compute change in SOC storage as SOC + dSOC, it's unclear
                 #   why, but we absolutely MUST test for NaNs here, not upstream
@@ -423,14 +468,21 @@ cdef void numerical_step(
 
 def load_state(config):
     '''
-    Populates global state variables with data. Note that, compared to other
-    respiration model variants, NPP here is the annual sum (i.e., not
-    divided by 365.)
+    Populates global state variables with data.
 
     Parameters
     ----------
     config : dict
         The configuration data dictionary
     '''
-    PFT[:] = np.fromfile(config['data']['PFT_map'], np.uint8)
-    ANNUAL_NPP[:] = np.fromfile(config['data']['NPP_annual_sum'], np.float32)
+    n_bytes = sizeof(unsigned char)*SPARSE_N
+    fid = open_fid(config['data']['PFT_map'].encode('UTF-8'), READ)
+    fread(PFT, sizeof(unsigned char), <size_t>n_bytes, fid)
+    fclose(fid)
+    if config['data']['NPP_annual_sum'] != '':
+        fid = open_fid(config['data']['NPP_annual_sum'].encode('UTF-8'), READ)
+        fread(AVAIL_NPP, sizeof(float), <size_t>n_bytes, fid)
+        fclose(fid)
+        for i in range(SPARSE_N):
+            # Available NPP is 1/365th of the annual NPP sum
+            AVAIL_NPP[i] = AVAIL_NPP[i] / 365
